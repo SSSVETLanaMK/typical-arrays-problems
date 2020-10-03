55be8aa053d8272b0e3aa4ee15ad770a394ad27a
Svetlana Makridina <sssmakovka@gmail.com>
2020-10-03 17:58:15 +0300

    feat: wrote the logic of the functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+
+  return Math.min.apply(Math.min, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+  return Math.max.apply(Math.max, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0; 
+  }
+
+  if (array.length == 1) {
+      return array[0];
+  } else {
+      let sum = 0;
+      for(let i = 0; i <= array.length - 1; i++) {
+        sum += array[i];
+    }
+      return sum/array.length;
+  }
 }
